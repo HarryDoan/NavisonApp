54fd9a3124811e934a757a74c945240b12dee0d0
Harry Doan <doankhanhdong3105@gmail.com>
2023-10-07 17:02:19 +0700

    update code

--- src/screens/common/WifiConfigScreen.tsx
+++ src/screens/common/WifiConfigScreen.tsx
@@ -1,4 +1,10 @@
 // WifiConfigScreen.js
+import {icons} from '@assets';
+import {Block, Image, Pressable} from '@components';
+import HeaderHome from '@components/common/HeaderHome';
+import {root} from '@navigation/NavigationRef';
+import {COLORS} from '@theme';
+import {height, width} from '@utils/responses';
 import axios from 'axios';
 import React, {useState} from 'react';
 import {View, TextInput, Button} from 'react-native';
@@ -21,20 +27,50 @@ const WifiConfigScreen = ({}) => {
   };
 
   return (
-    <View>
-      <TextInput
-        placeholder="SSID"
-        onChangeText={text => setSsid(text)}
-        value={ssid}
-      />
-      <TextInput
-        placeholder="Password"
-        onChangeText={text => setPassword(text)}
-        value={password}
-        secureTextEntry={true}
-      />
-      <Button title="Send" onPress={sendWifiConfig} />
-    </View>
+    <Block paddingVertical={15} paddingHorizontal={15}>
+      <Pressable
+        onPress={() => root.goBack()}
+        justifyCenter
+        alignCenter
+        width={40}
+        height={40}
+        radius={40}
+        backgroundColor={COLORS.bg_primary}>
+        <Image source={icons.ic_go_back} square={25} />
+      </Pressable>
+      <Block
+        style={{
+          marginTop: 15,
+          borderRadius: 10,
+          paddingVertical: 15,
+          paddingHorizontal: 15,
+          backgroundColor: COLORS.gray,
+        }}>
+        <TextInput
+          style={{
+            backgroundColor: 'white',
+            paddingHorizontal: 10,
+            borderRadius: 5,
+          }}
+          placeholder="SSID"
+          onChangeText={text => setSsid(text)}
+          value={ssid}
+        />
+        <TextInput
+          style={{
+            backgroundColor: 'white',
+            paddingHorizontal: 10,
+            borderRadius: 5,
+            marginVertical: 15,
+          }}
+          placeholder="Password"
+          onChangeText={text => setPassword(text)}
+          value={password}
+          secureTextEntry={true}
+        />
+        <Button title="Send" onPress={sendWifiConfig} />
+      </Block>
+    </Block>
   );
 };
 
